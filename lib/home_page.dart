0a48b23b185a9c4a062e3740e05001dd7a34d59a
Jorgejpc <jorgepc9531@gmail.com>
2020-10-27 11:11:09 -0400

    Initial commit

---
+++ lib/home_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_app/authentication_service.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  final User user;
+
+  const HomePage({Key key, this.user}) : super(key: key);
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: _scaffoldKey,
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              child: Text(
+                widget.user.email,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+            ),
+            Container(
+              child: OutlineButton(
+                child: Text("LogOut"),
+                onPressed: () {
+                  _signOut().whenComplete(() {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(
+                        builder: (context) => FirebaseAuthDemo()));
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future _signOut() async {
+    await _auth.signOut();
+  }
+}
